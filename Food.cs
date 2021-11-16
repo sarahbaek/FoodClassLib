f1d18e04ace475da5c1b73026564b5d65189421f
sarahBæk2021 <sbaek18@gmail.com>
2021-11-16 10:27:08 +0100

    Add project files.

---
+++ Food.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FoodClassLib
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int InStock { get; set; }
+        public int LowLevel { get; set; }
+        public string TypeOfFood { get; set; }
+        public Food()
+        {
+            
+        }
+
+        public Food(int id, string name, int inStock, int lowLevel)
+        {
+            Id = id;
+            Name = name;
+            InStock = inStock;
+            LowLevel = lowLevel;
+        }
+
+        public Food(string typeOfFood)
+        {
+            TypeOfFood = typeOfFood;
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}, Name = {Name}, " +
+                   $"InStock= {InStock}," +
+                   $" LowLevel = {LowLevel}";
+
+        }
+
+    }
+}
